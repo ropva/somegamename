5328a0c4121fa456356fc5b3b8f7b8a9dde13c79
Unto <developer.hekinav@gmail.com>
2026-09-23 20:14:06 +0300

    started working on tile system

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,14 @@
 extends Node
 
 var money = 0
+
+class Tile:
+	var id = ""
+	var sprite = ""
+	func _init(_id: String, _sprite: String):
+		id = _id
+		sprite = _sprite
+
+var tiles = [
+	Tile.new("solar_plant", "asset_solar_plant_1x")
+]
